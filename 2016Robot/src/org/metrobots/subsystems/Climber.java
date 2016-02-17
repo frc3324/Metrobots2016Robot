0aa5d0e8e6f76dfefe94c9bfb10d415d39b22eaf
tasgoon <tasgo@outlook.com>
2016-02-17 17:36:42 -0500

    test

---
+++ 2016Robot/src/org/metrobots/subsystems/Climber.java
@@ -0,0 +1,12 @@
+package org.metrobots.subsystems;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Climber extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
